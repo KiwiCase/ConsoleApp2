496ee40803a697a0b15a47a362676d13256c92a9
Kiwicase <emailcaseydent@gmail.com>
2022-06-29 17:53:46 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int randomNumber = rnd.Next(1, 101);
+            int guess = 0;
+
+            Console.WriteLine("Guess a number between 1-100");
+
+            if (rnd.Next(1) == guess)
+            {
+                Console.WriteLine("Correct! You win!");
+            }
+
+            if (rnd.Next(1) > guess)
+            { 
+                Console.WriteLine("Too high...")
+            }
+
+            else
+            { 
+                Console.WriteLine("Too low...");
+            }
+
+        }
+    }
+}
