dadc3fa9471412aa849e06cf7357bcc6d993012c
Kiwicase <emailcaseydent@gmail.com>
2022-06-30 13:24:59 +0300

    Random Number Guessing Game - In progress

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,29 +6,32 @@
         {
             Random rnd = new Random();
 
-            int randomNumber = rnd.Next(1, 101);
+            int randomNumber = rnd.Next(100) + 1;
             int guess = 0;
-            int guesses = 0;
+
+            while (guess != randomNumber)
+            {
 
             Console.WriteLine("Guess a number between 1-100:");
             int.TryParse(Console.ReadLine(), out guess);
            
-
-            if (randomNumber == guess)
+            
+            if (guess < randomNumber)
             {
-                Console.WriteLine("Correct! You win!");
+                Console.WriteLine("Too low...", guess);
             }
 
-            if (rnd.Next(1) > guess)
+            if (guess > randomNumber)
             {
-                Console.WriteLine("Too high...");
+                Console.WriteLine("Too high...", guess);
             }
 
-            else
+            if (guess == randomNumber)
             { 
-                Console.WriteLine("Too low...");
+                Console.WriteLine("Correct! You win!");
 
-             
+            }
+            
             }
 
         }
