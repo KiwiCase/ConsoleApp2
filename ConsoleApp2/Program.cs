1549a521bbc3bafe522a931edfbfc36c649b81bc
Kiwicase <emailcaseydent@gmail.com>
2022-07-11 17:46:48 +0300

    High/Low Number Guessing - added option to play again or not and limited number of guesses

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,38 +5,73 @@
         static void Main(string[] args)
         {
             Random rnd = new Random();
-
+            bool playAgain = true;
             int randomNumber = rnd.Next(100) + 1;
             int guess = 0;
+            var count = 0;
+            String response;
 
-            while (guess != randomNumber)
+            while (playAgain)
             {
+                guess = 0;
+                count = 0;
+                response = "";
+                randomNumber = rnd.Next(100) + 1;
+ 
 
-            Console.WriteLine("Guess a number between 1-100:");
+                while (guess != randomNumber)
+                    {
+
+                    Console.WriteLine("Guess a number between 1-100:");
             
                 while (!int.TryParse(Console.ReadLine(), out guess))
                 {
                     Console.WriteLine("Invalid input. Please enter a number.");
                 }
 
-            if (guess < randomNumber)
-            {
-                Console.WriteLine("Too low...", guess);
-            }
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Too low...", guess);
+                }
 
-            if (guess > randomNumber)
-            {
-                Console.WriteLine("Too high...", guess);
-            }
+                if (guess > randomNumber)
+                {
+                    Console.WriteLine("Too high...", guess);
+                }
+                count++;
+                if (count == 5)
+                {
+                    Console.WriteLine("Too many guesses! Press Y to start again or N to quit.");
+                    response = Console.ReadLine().ToUpper();
+                        if (response == "Y")
+                        {
+                            playAgain = true;
+                        }
+                        else
+                        {
+                            playAgain = false;
+                        }
+                    }
+                if (guess == randomNumber)
+                { 
+                    Console.WriteLine("Correct! You win!");
+                    Console.WriteLine("Press Y to play again or N to quit.");
+                    response = Console.ReadLine().ToUpper();
 
-            if (guess == randomNumber)
-            { 
-                Console.WriteLine("Correct! You win!");
-
-            }
+                        if (response == "Y")
+                        {
+                            playAgain = true;
+                        }
+                        else
+                        {
+                            playAgain = false;
+                        }
+                }
+                
+                }
             
             }
-
+                    Console.WriteLine("What a scardee cat! Bye!");
         }
     }
 }
