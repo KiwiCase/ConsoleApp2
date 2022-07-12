abb1bbc05ed2d921c77ff9f1de43b4999b34feaf
Kiwicase <emailcaseydent@gmail.com>
2022-07-12 18:19:03 +0300

    High/Low Number Guessing Game - unsuccessful in Y or N only input for Play Again?

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -58,6 +58,7 @@
                         if (response == "N")
                             {
                             playAgain = false;
+                            break;
                             }
                     }
 
